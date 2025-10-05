2118e0a5e66acbaaf8c7e08c707a88e35f2df059
HuyPham <phamminhhuy0901tk@gmail.com>
2025-10-05 18:33:52 +0700

    Add Listener

--- BT_Chuyen_Activity/app/src/main/java/com/example/activity/Profile.java
+++ BT_Chuyen_Activity/app/src/main/java/com/example/activity/Profile.java
@@ -1,6 +1,8 @@
 package com.example.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Profile extends AppCompatActivity {
-
+    private TextView txtView,txtProfileName,txtViewEmail;
+    final String userName = "tenNguoiDung";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,17 @@ public class Profile extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        txtView=findViewById(R.id.txtView);
+        txtView.setOnClickListener(view -> {
+            Intent intent= new Intent(this,MainActivity.class);
+            startActivity(intent);
+        });
+        txtProfileName=findViewById(R.id.profileUserName);
+        txtViewEmail=findViewById(R.id.txtViewId);
+        String getPutExtra=getIntent().getStringExtra(userName);
+        if(getPutExtra!=null && !getPutExtra.isEmpty()){
+            txtProfileName.setText(getPutExtra);
+            txtViewEmail.setText("Email : "+ getPutExtra +"@gmail.com");
+        }
     }
 }
