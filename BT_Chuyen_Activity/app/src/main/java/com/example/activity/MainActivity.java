23eecd915e73d6740519bb40c31272f8d971e91c
HuyPham <phamminhhuy0901tk@gmail.com>
2025-10-05 18:33:52 +0700

    Add Listener

--- BT_Chuyen_Activity/app/src/main/java/com/example/activity/MainActivity.java
+++ BT_Chuyen_Activity/app/src/main/java/com/example/activity/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnClick;
+    private Boolean isPasswordVisible=false;
+    private ImageView imgViewPassword;
+    private EditText edt;
+    final String userName = "tenNguoiDung";
+    private EditText edtUserName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +32,25 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         btnClick=findViewById(R.id.btnClick);
+        edtUserName = findViewById(R.id.edtUserName);
         btnClick.setOnClickListener(view -> {
             Intent intent = new Intent(this,Profile.class);
+            intent.putExtra(userName,edtUserName.getText().toString());
             startActivity(intent);
+        });
+        imgViewPassword=findViewById(R.id.imgVisible);
+        edt=findViewById(R.id.editText);
+        imgViewPassword.setOnClickListener(view -> {
+            int cursorPosition = edt.getSelectionStart();
+            if(isPasswordVisible){
+                edt.setTransformationMethod(new PasswordTransformationMethod());
+                isPasswordVisible = false;
+            }
+            else{
+                edt.setTransformationMethod(new HideReturnsTransformationMethod());
+                isPasswordVisible=true;
+            }
+            edt.setSelection(cursorPosition);
         });
     }
 }
